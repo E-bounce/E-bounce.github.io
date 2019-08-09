7c7c25357efeb68f9862cc0a15b15ee27059896d
E-bounce <3199729962@qq.com>
2019-08-09 11:30:19 +0800

    Hexo Blog updated - 2019-08-09 11:30:09)

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 08 2019 19:03:00 GMT+0800 (GMT+08:00)
-$(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
+// build time:Fri Aug 09 2019 11:30:02 GMT+0800 (GMT+08:00)
+$(document).ready(function(){var rpath=window.location.href.replace(window.location.origin,"");var timeout;$(window).on("scroll",function(){clearTimeout(timeout);timeout=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+rpath,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var cvalues=Cookies.get("scroll-cookie").split("|");if(cvalues[1]===rpath){$(window).scrollTop(cvalues[0])}}});
 //rebuild by neat 
