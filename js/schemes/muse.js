5086e6ce4870713a23de98a83d0024254581f05f
E-bounce <3199729962@qq.com>
2019-09-05 17:27:30 +0800

    Hexo Blog updated - 2019-09-05 17:27:21)

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 09 2019 11:30:02 GMT+0800 (GMT+08:00)
+// build time:Thu Sep 05 2019 17:27:16 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function updateFooterPosition(){var containerHeight=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(containerHeight<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}updateFooterPosition();$(window).on("resize scroll",updateFooterPosition)});
 //rebuild by neat 
